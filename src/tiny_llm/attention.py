1979909888aecf3c85261e21084d55149fea1593
Michael Gu <michael80hou@icloud.com>
2026-09-14 00:00:51 +0800

    week1day3

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -95,16 +95,15 @@ def scaled_dot_product_attention_grouped(
 
     scores = mx.matmul(query, key.swapaxes(-2, -1)) * factor
     if mask is not None:
-        if isinstance(mask, str):
-            if mask != "causal":
-                raise ValueError(f"unsupported attention mask: {mask}")
+        if mask == "causal":
             if L > S:
                 raise ValueError("causal attention requires S >= L")
-            scores = scores + causal_mask(L, S, scores.dtype)
-        elif mask is not None:
-            mask = mx.broadcast_to(mask, (*B, H, 1, L, S))
+            mask = causal_mask(L, S, scores.dtype)
             scores = scores + mask
-
+        else:
+            mask = mx.broadcast_to(mask, (*B, H_q, L, S))
+            mask = mask.reshape(*B, 1, H, n_repeats, L, S)
+            scores = scores + mask
     output = mx.matmul(softmax(scores, axis=-1), value)
     return output.reshape(expected_shape)
 
